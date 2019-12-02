f40ffcdad6286649452641e36e7ce89de2366555
David El-Saig <daud.ici@gmail.com>
2019-12-02 17:34:34 +0100

    handle connection abort & minor cleanup

--- jquery-fetch.js
+++ jquery-fetch.js
@@ -1,7 +1,5 @@
-(function (ctx, error_fn_text, do_globals)
-{
-    function matchObects(a, b)
-    {
+(function (ctx, error_fn_text, do_globals) {
+    function matchObects(a, b) {
         if (!a || !b) return false;
         for (var x in a)
             if (a.hasOwnProperty(x))
@@ -10,12 +8,9 @@
         return true;
     }
 
-    function promiseFetchJQ(url, data, condition, xhr_callback)
-    {
-        return new Promise(function (resolve, reject)
-        {
-            var xhr = (data === undefined ? ctx.jQuery.get : ctx.jQuery.post)(url, data, function (r)
-            {
+    function promiseFetchJQ(url, data, condition, xhr_callback) {
+        return new Promise(function (resolve, reject) {
+            var xhr = (data === undefined ? ctx.jQuery.get : ctx.jQuery.post)(url, data, function (r) {
                 if (condition === undefined || condition === null)
                     resolve(r);
                 else if (typeof (condition) === 'function')
@@ -29,24 +24,18 @@
     }
 
 
-    function delay(duration)
-    {
-        return function (data)
-        {
-            return new Promise(function (resolve)
-            {
+    function delay(duration) {
+        return function (data) {
+            return new Promise(function (resolve) {
                 setTimeout(resolve, duration, data);
             });
         };
     }
 
 
-    function animate(options, duration, easing)
-    {
-        return function (x)
-        {
-            return new Promise(function (resolve, reject)
-            {
+    function animate(options, duration, easing) {
+        return function (x) {
+            return new Promise(function (resolve, reject) {
                 jQuery(x).animate(options,
                     {
                         duration: duration,
@@ -60,8 +49,7 @@
 
     var notify = {};
     notify = function () { return notify.callable.apply(this, arguments); };
-    notify.callable = function ()
-    {
+    notify.callable = function () {
         var text = Array.prototype.slice.apply(arguments).join(' ');
         var notify_box = jQuery('<div>')
             .attr('class', 'notification')
@@ -80,11 +68,9 @@
 
     var ctx2 = "Notification" in ctx ? ctx : "Notification" in window ? window : null;
     if (ctx2) ctx2.Notification.requestPermission()
-            .then(function (permission)
-            {
+            .then(function (permission) {
                 if (permission === "granted")
-                    notify.callable = function (text)
-                    {
+                    notify.callable = function (text) {
                         text = text || '';
                         var t = text.split('\n');
                         var o = {};
@@ -95,49 +81,53 @@
             .catch(function (x) { notify(x); });
 
 
-    function error_fn(fn, e)
-    {
-        if (!e)
-            fn(error_fn_text);
-        else
-        {
-            var error;
+    function error_fn(fn, e) {
+        if (e.statusText === 'abort') return;
 
-            if (typeof (e) === 'string' || typeof (e) === 'number')
-                error = e;
-            else if (typeof (e) === 'object' && (e instanceof Error || ('message' in e && 'stack' in e && 'fileName' in e)))
-            {
-                error = e.toString() + '\n';
-                error += 'in: ' + e.fileName.replace(/^(?:\/\/|[^\/]+)*\//, "/") + '\n'
-                error += 'at: ' + e.lineNumber + ' : ' + e.columnNumber + '\n';
-                error += e.stack;
-            }
-            else if ('getResponseHeader' in e && (e.getResponseHeader('Content-Type') || '').indexOf('html') >= 0 && 'responseText' in e)
+        if (!e) {
+            fn(error_fn_text);
+            return;
+        }
+
+        var error;
+
+        if (typeof (e) === 'string' || typeof (e) === 'number')
+            error = e;
+        else if (typeof (e) === 'object' && (e instanceof Error || ('message' in e && 'stack' in e && 'fileName' in e))) {
+            error = e.toString() + '\n';
+            error += 'in: ' + e.fileName.replace(/^(?:\/\/|[^\/]+)*\//, "/") + '\n'
+            error += 'at: ' + e.lineNumber + ' : ' + e.columnNumber + '\n';
+            error += e.stack;
+        }
+        else if ('getResponseHeader' in e) {
+            if ((e.getResponseHeader('Content-Type') || '').indexOf('html') >= 0 && 'responseText' in e)
                 error = new DOMParser()
                     .parseFromString(e.responseText, 'text/html')
                     .querySelector('title')
                     .textContent
                     .trim();
-            else if ('error' in e)
-                error = e.error;
-            else
-                error = JSON.stringify(e);
-
-            console.log(e);
-            console.log(error);
-            fn(error_fn_text + '\n\n' + error);
+            else {
+                fn(error_fn_text);
+                return;
+            }
         }
+        else if ('error' in e)
+            error = e.error;
+        else
+            error = JSON.stringify(e);
+
+        console.log(e);
+        console.log(error);
+        fn(error_fn_text + '\n\n' + error);
 
         return e;
     }
     function error_alert(e) { error_fn(alert, e); throw e; }
     function error_notify(e) { error_fn(notify, e); throw e; }
     function error_absorb(e) { console.log(e); }
-    function error_makeAlertWithId(error_id)
-    {
+    function error_makeAlertWithId(error_id) {
         var id = ("0000" + error_id).slice(-4);
-        return function (e)
-        {
+        return function (e) {
             alert("ERROR #" + id + "\n" + (typeof e === 'string' ? e : JSON.stringify(e, null, 2)));
         };
     }
@@ -145,6 +135,7 @@
     ctx.jQuery.fetch = function () { return ctx.jQuery.fetch.callable.apply(this, arguments); };
     ctx.jQuery.fetch.callable = promiseFetchJQ;
     ctx.jQuery.fetch.delay = delay;
+    ctx.jQuery.fetch.ignoreStatus = [];
     ctx.jQuery.fetch.animate = animate;
     ctx.jQuery.fetch.notify = notify;
     ctx.jQuery.fetch.error = {
@@ -155,8 +146,7 @@
         make: { alertWithId: error_makeAlertWithId }
     };
 
-    if (do_globals)
-    {
+    if (do_globals) {
         ctx.delay = delay;
         ctx.animate = animate;
         ctx.notify = notify;
